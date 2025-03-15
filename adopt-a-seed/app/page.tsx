f0585febdce23d290c9254a526e9c5e98df84d12
PowerOfCreation <questerdesura@gmail.com>
2025-03-15 23:15:36 +0100

    style: improved styling of start page and layout.tsx

--- adopt-a-seed/app/page.tsx
+++ adopt-a-seed/app/page.tsx
@@ -5,9 +5,9 @@ import { PlantsCarousel } from "./view-plants/components/plants-carousel";
 
 export default function Home() {
   return (
-    <>
+    <div className="container mx-auto p-4">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Plants</h1>
       <PlantsCarousel plants={getPlantsDummyData}></PlantsCarousel>
-    </>
+    </div>
   );
 }
