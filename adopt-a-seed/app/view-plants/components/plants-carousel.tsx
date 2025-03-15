d7363d2470686143fae49a5bbec1a62329a81230
PowerOfCreation <questerdesura@gmail.com>
2025-03-15 20:25:44 +0100

    feat: changed to scrollarea

--- adopt-a-seed/app/view-plants/components/plants-carousel.tsx
+++ adopt-a-seed/app/view-plants/components/plants-carousel.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { PlantCard } from "@/components/plant-card";
-import { Carousel, CarouselContent } from "@/components/ui/carousel";
+import { ScrollBar } from "@/components/ui/scroll-area";
 import { Plant } from "@/lib/plant";
+import { ScrollArea } from "@radix-ui/react-scroll-area";
 import { Plus } from "lucide-react";
 import Link from "next/link";
 
@@ -12,9 +13,9 @@ interface PlantsCarouselProps {
 
 export function PlantsCarousel({ plants }: PlantsCarouselProps) {
   return (
-    <Carousel>
-      <CarouselContent className="-ml-4">
-        <div className="pl-4">
+    <ScrollArea className="whitespace-nowrap overflow-auto">
+      <div className="flex w-max space-x-4 p-4">
+        <div className="shrink-0">
           <Link href="/start-growing-process">
             <div className="border rounded-md p-4 w-48 h-64 flex items-center justify-center">
               <Plus size={64} />
@@ -22,11 +23,12 @@ export function PlantsCarousel({ plants }: PlantsCarouselProps) {
           </Link>
         </div>
         {plants.map((plant, index) => (
-          <div key={index} className="pl-4">
+          <div key={index} className="shrink-0 overflow-hidden">
             <PlantCard plant={plant} />
           </div>
         ))}
-      </CarouselContent>
-    </Carousel>
+      </div>
+      <ScrollBar orientation="horizontal" />
+    </ScrollArea>
   );
 }
