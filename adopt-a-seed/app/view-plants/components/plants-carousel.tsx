01a9397a7a36a090a8003687e7f5d2f10b7c202e
PowerOfCreation <questerdesura@gmail.com>
2025-03-15 20:07:35 +0100

    feat: add plus card

--- adopt-a-seed/app/view-plants/components/plants-carousel.tsx
+++ adopt-a-seed/app/view-plants/components/plants-carousel.tsx
@@ -3,6 +3,8 @@
 import { PlantCard } from "@/components/plant-card";
 import { Carousel, CarouselContent } from "@/components/ui/carousel";
 import { Plant } from "@/lib/plant";
+import { Plus } from "lucide-react";
+import Link from "next/link";
 
 interface PlantsCarouselProps {
   plants: Plant[];
@@ -12,6 +14,13 @@ export function PlantsCarousel({ plants }: PlantsCarouselProps) {
   return (
     <Carousel>
       <CarouselContent className="-ml-4">
+        <div className="pl-4">
+          <Link href="/start-growing-process">
+            <div className="border rounded-md p-4 w-48 h-64 flex items-center justify-center">
+              <Plus size={64} />
+            </div>
+          </Link>
+        </div>
         {plants.map((plant, index) => (
           <div key={index} className="pl-4">
             <PlantCard plant={plant} />
