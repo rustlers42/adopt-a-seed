1eb0a1a953bace02ed7dbed0fb47f4ef52f4dd33
PowerOfCreation <questerdesura@gmail.com>
2025-03-16 18:44:27 +0100

    feat: move help-alert to new component and added markdown support

--- adopt-a-seed/app/view-grow-information/[id]/PlantPage.tsx
+++ adopt-a-seed/app/view-grow-information/[id]/PlantPage.tsx
@@ -7,16 +7,12 @@ import { useFetchApi } from "@/lib/use-api";
 import { PlantDTO } from "@/lib/plant";
 import EventList from "@/app/view-events/components/event-list";
 import { EventDTO } from "@/lib/event";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { TreesIcon as Plant } from "lucide-react";
+import HelpAlert from "@/app/view-grow-information/components/help-alert";
+import ProtectedRoute from "@/components/protected-route";
 
 interface PlantPageProps {
   id: string;
 }
-
-type HelpResponseDTO = {
-  text: string;
-};
 
 export default function PlantPage({ id }: PlantPageProps) {
   const { data: plant, isLoading } = useFetchApi<PlantDTO>(`http://localhost:8000/plants/${id}`, {
@@ -29,26 +25,16 @@ export default function PlantPage({ id }: PlantPageProps) {
     enabled: true,
   });
 
-  const { data: help } = useFetchApi<HelpResponseDTO>(`http://localhost:8000/plants/${id}/help`, {
-    requireAuth: true,
-    enabled: true,
-  });
-
   return (
-    <div className={"container mx-auto py-8"}>
-      {help && (
-        <Alert className="bg-green-50 border-green-200">
-          <Plant className="h-4 w-4 text-green-600" />
-          <AlertTitle>Help</AlertTitle>
-          <AlertDescription>{help?.text}</AlertDescription>
-        </Alert>
-      )}
+    <ProtectedRoute>
+      <div className={"container mx-auto py-8"}>
+        <HelpAlert id={id}></HelpAlert>
+        <h1>Plant Information</h1>
+        <PlantDetails plant={plant} />
+        <PlantSurvey />
 
-      <h1>Plant Information</h1>
-      <PlantDetails plant={plant} />
-      <PlantSurvey />
-
-      <EventList events={events}></EventList>
-    </div>
+        <EventList events={events}></EventList>
+      </div>
+    </ProtectedRoute>
   );
 }
---
+++ adopt-a-seed/app/view-grow-information/components/help-alert.tsx
@@ -0,0 +1,34 @@
+﻿import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { useFetchApi } from "@/lib/use-api";
+import { TreesIcon as Plant } from "lucide-react";
+import React from "react";
+import ReactMarkdown from "react-markdown";
+
+interface PlantPageProps {
+  id: string;
+}
+
+type HelpResponseDTO = {
+  text: string;
+};
+
+export default function HelpAlert({ id }: PlantPageProps) {
+  const { data: help } = useFetchApi<HelpResponseDTO>(`http://localhost:8000/plants/${id}/help`, {
+    requireAuth: true,
+    enabled: true,
+  });
+
+  return (
+    <>
+      {help && (
+        <Alert className="bg-green-50 border-green-200">
+          <Plant className="h-4 w-4 text-green-600" />
+          <AlertTitle>Help</AlertTitle>
+          <AlertDescription>
+            <ReactMarkdown>{help.text}</ReactMarkdown>
+          </AlertDescription>
+        </Alert>
+      )}
+    </>
+  );
+}
