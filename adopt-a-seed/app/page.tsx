44318ba3ebc8e3d10ae1c808deb1e436ba602543
lks-hrsch <lkshrsch2001@outlook.com>
2025-03-16 16:47:48 +0100

    refactor: improve component structure and variable handling in page components

--- adopt-a-seed/app/page.tsx
+++ adopt-a-seed/app/page.tsx
@@ -1,7 +1,11 @@
-export default function () {
+function Page() {
   return (
     <div>
       <h1>Page</h1>
     </div>
   );
 }
+
+Page.displayName = "Page";
+
+export default Page;
--- adopt-a-seed/app/view-seed-database/page.tsx
+++ adopt-a-seed/app/view-seed-database/page.tsx
@@ -1,18 +1,20 @@
 "use client";
 
+import { SeedDatabaseDTO } from "@/lib/seed-database";
+import { useFetchApi } from "@/lib/use-api";
 import { columns } from "./components/ui/columns";
 import { DataTable } from "./components/ui/data-table";
-import { useFetchApi } from "@/lib/use-api";
-import { SeedDatabaseDTO } from "@/lib/seed-database";
 
 export default function ViewSeedDatabasePage() {
-  let { data: entries, isLoading } = useFetchApi<SeedDatabaseDTO[]>(`http://localhost:8000/seed_databases/seeds`, {
+  const { data: entries, isLoading } = useFetchApi<SeedDatabaseDTO[]>(`http://localhost:8000/seed_databases/seeds`, {
     requireAuth: false,
     enabled: true,
   });
 
+  let expandedEntries = entries;
+
   if (!isLoading && entries) {
-    const expandedEntries = [];
+    expandedEntries = [];
 
     for (const entry of entries) {
       if (entry.seeds.length === 0) {
@@ -24,13 +26,11 @@ export default function ViewSeedDatabasePage() {
         expandedEntries.push({ ...entry, seeds: [seed] });
       }
     }
-
-    entries = expandedEntries;
   }
 
   return (
     <div className="container mx-auto py-10">
-      <DataTable columns={columns} data={entries} />
+      <DataTable columns={columns} data={expandedEntries} />
     </div>
   );
 }
