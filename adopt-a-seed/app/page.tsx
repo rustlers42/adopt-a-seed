94802c444968357e9741da1098da08f298f95b06
PowerOfCreation <questerdesura@gmail.com>
2025-03-16 11:10:42 +0100

    feat: refactor page structure to use PlantPage component and integrate API for plant data

--- adopt-a-seed/app/page.tsx
+++ adopt-a-seed/app/page.tsx
@@ -1,15 +1,27 @@
 "use client";
 
-import { getPlantsDummyData } from "@/lib/plant";
 import { PlantsCarousel } from "./view-plants/components/plants-carousel";
 import ProtectedRoute from "@/components/protected-route";
+import { useFetchApi } from "@/lib/use-api";
+
+export type PlantDTO = {
+  id: number;
+  seed_category: string;
+  seed_specific: string;
+  planted_at: string;
+};
 
 export default function Home() {
+  const { data: plants, isLoading } = useFetchApi<PlantDTO[]>("http://localhost:8000/plants", {
+    requireAuth: true,
+    enabled: true,
+  });
+
   return (
     <ProtectedRoute>
       <div className="container mx-auto p-4">
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Plants</h1>
-        <PlantsCarousel plants={getPlantsDummyData}></PlantsCarousel>
+        {<PlantsCarousel plants={plants}></PlantsCarousel>}
       </div>
     </ProtectedRoute>
   );
--- adopt-a-seed/app/view-grow-information/[id]/page.tsx
+++ adopt-a-seed/app/view-grow-information/[id]/page.tsx
@@ -1,20 +1,7 @@
-import { getPlantById } from "../../../lib/plant";
-import PlantDetails from "./PlantDetails";
-import PlantSurvey from "./PlantSurvey";
+import PlantPage from "./PlantPage";
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
-  const plant = getPlantById(Number(id));
 
-  if (!plant) {
-    return <div>Plant not found</div>;
-  }
-
-  return (
-    <div>
-      <h1>Plant Information</h1>
-      <PlantDetails plant={plant} />
-      <PlantSurvey />
-    </div>
-  );
+  return <PlantPage id={id} />;
 }
